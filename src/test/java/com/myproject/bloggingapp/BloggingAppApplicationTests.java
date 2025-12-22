43da1390d808cd52b705ddf222538dfd99b6b741
SURIYA S <sujasuriya2306@gmail.com>
2025-12-22 10:42:21 +0530

    Initial commit

---
+++ src/test/java/com/myproject/bloggingapp/BloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.bloggingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
